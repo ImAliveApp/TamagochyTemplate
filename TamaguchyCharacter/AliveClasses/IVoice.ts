5911a9b48091ffe157efdc7c920264341db297c5
hay12396 <hay12396@gmail.com>
2017-01-23 13:30:58 +0200

    .

--- TamaguchyCharacter/AliveClasses/IVoice.ts
+++ TamaguchyCharacter/AliveClasses/IVoice.ts
@@ -1,6 +1,9 @@
 ﻿interface IVoice {
     getLatency(): number;
     getLanguage(): string;
+    getISO3Language(): string;
+    getCountry(): string;
+    getISO3Country(): string;
     getQuality(): number;
     getName(): string;
     isNetworkConnectionRequired(): boolean;
