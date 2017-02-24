2b5c2a87ae22673e205d4dc9a205c36473baf19a
hay12396 <hay12396@gmail.com>
2017-02-24 09:41:24 +0200

    .

--- TamagochyTemplate/app.js
+++ TamagochyTemplate/app.js
@@ -6,15 +6,15 @@ var AliveClass = (function () {
         this.lastDrawTime = 0;
         this.lastPlaySoundTime = 0;
     }
-    AliveClass.prototype.onStart = function (mHandler, disabledPermissions) {
-        this.actionManager = mHandler.getActionManager();
-        this.resourceManager = mHandler.getResourceManager();
-        this.databaseManager = mHandler.getDatabaseManager();
-        this.characterManager = mHandler.getCharacterManager();
-        this.menuManager = mHandler.getMenuManager();
-        this.configurationMananger = mHandler.getConfigurationManager();
-        this.restManager = mHandler.getRestManager();
-        this.awarenessManager = mHandler.getAwarenessManager();
+    AliveClass.prototype.onStart = function (handler, disabledPermissions) {
+        this.actionManager = handler.getActionManager();
+        this.resourceManager = handler.getResourceManager();
+        this.databaseManager = handler.getDatabaseManager();
+        this.characterManager = handler.getCharacterManager();
+        this.menuManager = handler.getMenuManager();
+        this.configurationMananger = handler.getConfigurationManager();
+        this.restManager = handler.getRestManager();
+        this.awarenessManager = handler.getAwarenessManager();
         this.resourceManagerHelper = new ResourceManagerHelper(this.resourceManager);
         this.actionManager.move(0, this.configurationMananger.getScreenHeight(), 0);
         this.resizeRatio = this.configurationMananger.getMaximalResizeRatio();
